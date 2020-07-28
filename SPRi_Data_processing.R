57e7c6965896f2926f405a871d8178a8514b0d3b
Bram Ratz <bratz@uoguelph.ca>
2020-07-28 10:55:42 -0400

    Update SPRi_Data_processing.R
    further edits

--- SPRi_Data_processing.R
+++ SPRi_Data_processing.R
@@ -5,10 +5,6 @@
 
 ##################################################
 "
-Goal here is to address some of the issues I see in
-the current data munging process in Greg's approach
-and to see if it is possible to largely automate
-this process from start to finish in R itself.
 
 Quick overview of the new data munging process:
   1 - Average all the spots together
